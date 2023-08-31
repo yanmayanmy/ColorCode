a7d86bc02cafc00f1f1e1b58d9c063f50aba04e3
yanmayanmy <yanmy.yamauchi@karabiner.tech>
2023-08-31 15:29:57 +0900

    [add] Playing around with the inheritance of Objects.

--- tutorial.js
+++ tutorial.js
@@ -1,1 +1,13 @@
-console.log('hello')
+const human = {
+    kind: 'Human'
+}
+
+const me = Object.create(human)
+
+me.age = 24
+me.gender = 'male'
+
+const you = Object.create(me)
+you.age = 20
+
+console.log(you)
