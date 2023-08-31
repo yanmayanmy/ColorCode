c65cba1668f02ae33ae9e4c10dd41102a12c0452
yanmayanmy <yanmy.yamauchi@karabiner.tech>
2023-08-31 17:03:10 +0900

    [add] Learned about constructor.

--- tutorial.js
+++ tutorial.js
@@ -1,1 +1,15 @@
-console.log('hello')
+function SuperElement(type, content) {
+    this.el = document.createElement(type)
+    this.el.innerText = content
+    document.body.append(this.el)
+    this.el.addEventListener('click', () => {
+        console.log(this.el)
+    })
+}
+
+const h1 = SuperElement('h1', 'Hello World!')
+
+const array = ['a', 'b', 'c']
+const myElements = array.map(item => {
+    return new SuperElement('p', item)
+})
