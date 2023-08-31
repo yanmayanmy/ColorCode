4372dd8458438b55b6ee9294287234a547f8bcb3
yanmayanmy <yanmy.yamauchi@karabiner.tech>
2023-08-31 10:35:52 +0900

    [add] inheritance.js

---
+++ inheritance.js
@@ -0,0 +1,14 @@
+const me = {
+    talk() {
+        return 'Talking';
+    }
+}
+
+const you = {
+    talk() {
+        return 'Talking';
+    }
+}
+
+console.log(me.talk());
+console.log(you.talk());
