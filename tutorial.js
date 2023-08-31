717bbc312eeb2f410efaa1985695449059540826
yanmayanmy <yanmy.yamauchi@karabiner.tech>
2023-08-31 16:31:49 +0900

    [add] Learned the basics about factory.

--- tutorial.js
+++ tutorial.js
@@ -1,1 +1,22 @@
-console.log('hello')
+function createElement(type, text, color) {
+    const el = document.createElement(type)
+    el.innerText = text
+    el.style.color = color
+    document.body.append(el)
+    return {
+        el, 
+        setText(text) {
+            el.innerText = text
+        },
+        setColor(color) {
+            el.style.color = color
+        }
+    }
+}
+
+const h1 = createElement('h1', 'hey guys', 'red')
+const p = createElement('p', 'sup!!', 'green')
+
+console.log(h1)
+
+h1.setText('Good Bye!')
