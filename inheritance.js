784df495c591805d18131024a92efbfbef0464d3
yanmayanmy <yanmy.yamauchi@karabiner.tech>
2023-08-31 10:44:37 +0900

    [fix] Code duplication. #1
    Used a Person class to solve the code duplication.

--- inheritance.js
+++ inheritance.js
@@ -1,14 +1,26 @@
-const me = {
+// const me = {
+//     talk() {
+//         return 'Talking';
+//     }
+// }
+
+// const you = {
+//     talk() {
+//         return 'Talking';
+//     }
+// }
+
+// console.log(me.talk());
+// console.log(you.talk());
+
+class Person {
     talk() {
-        return 'Talking';
+        return 'Talking'
     }
 }
 
-const you = {
-    talk() {
-        return 'Talking';
-    }
-}
+const me = new Person();
+const you = new Person();
 
 console.log(me.talk());
 console.log(you.talk());
